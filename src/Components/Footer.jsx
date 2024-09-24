49e3b91d0db74f87e1dc553c27f485a2094f99c7
DevFaith <waitherafaith@outlook.com>
2024-09-25 01:38:55 +0300

    * feat(Footer.jsx): add Footer component to project

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-black text-white py-4 px-8 text-center">
+      <p>&copy; 2024 SmartHome | All Rights Reserved</p>
+      <p className="text-sm mt-2">Contact: support@smarthome.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
